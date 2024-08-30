54bd00bf4facbb673070b0caf8b6d9968776ca1f
unknown <off_players@mail.ru>
2024-08-30 19:09:47 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+echo 'hello world';
+echo 'hello world 2';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>when2run</title>
+</head>
+<body data-bs-theme="dark">
+    <div class="center container">
+        <div class="center container">
+            <h1>
+                <img src="img/logo_w2r.png">
+            </h1>
+        </div>
+        <div class="center container w-90">
+            <ul class="list-group">
+                <li class="d-flex list-group-item btn">
+
+                        <div class="flex-grow-1 m-1 p-3 border border-danger">Лево</div>
+                        <div class="m-1 d-flex flex-column">
+                            <div class="p-3 border border-danger"><i class="bi bi-pencil-square m-2 fs-2 text-info-emphasis"></i></div>
+                            <div class="p-3 border border-danger"><i class="bi bi-trash m-2 fs-2 text-danger"></i></div>
+                        </div>
+
+                </li>
+                <li class="list-group-item">Второй элемент</li>
+                <li class="list-group-item">Третий элемент</li>
+                <li class="list-group-item">Четвертый элемент</li>
+                <li class="list-group-item">И пятый</li>
+            </ul>
+        </div>
+    </div>
+<script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
